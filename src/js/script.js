1b411d5fd574f4121952c48af4e0bbc738b247f4
Dmitriy <dmitrijkluchnik47@gmail.com>
2024-07-08 23:53:29 +0300

    added contacts block

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', ()=> {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__ratings-pct'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
